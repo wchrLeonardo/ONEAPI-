e26588a4fc9be6b444102b3672169481f70de924
Leonardo Wicher <leonardo.wlferreira@gmail.com>
2024-10-10 22:23:04 -0300

    Consumo de API de ONE PIECE

--- src/pages/bounties.js
+++ src/pages/bounties.js
@@ -1,25 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react"
 
 export default function Characters() {
-  const [characters, setCharacters] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const selectedCharacters = [258, 289, 187, 96, 85, 247, 81, 32, 1, 54, 59, 82]; // IDs dos personagens que você deseja mostrar
+  const [characters, setCharacters] = useState([])
+  const [loading, setLoading] = useState(true)
+  const selectedCharacters = [
+    258, 289, 187, 96, 85, 247, 81, 32, 1, 54, 59, 82,
+  ]; // IDs dos personagens que você deseja mostrar
 
   useEffect(() => {
-    const url = 'https://api.api-onepiece.com/v2/characters/en';
+    const url = "https://api.api-onepiece.com/v2/characters/en"
 
     const fetchData = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch(url)
         if (!response.ok) {
-          throw new Error('Erro na requisição');
+          throw new Error("Erro na requisição")
         }
-        const result = await response.json();
+        const result = await response.json()
         console.log(result); // Verifique o que está sendo retornado
         setCharacters(result);
         setLoading(false);
       } catch (error) {
-        console.error('Erro ao buscar os dados:', error);
+        console.error("Erro ao buscar os dados:", error)
         setLoading(false);
       }
     };
@@ -28,106 +30,72 @@ export default function Characters() {
   }, []);
 
   if (loading) {
-    return <p>Carregando...</p>;
+    return (
+      <div className="flex items-center justify-center min-h-screen relative">
+      <img src="/images/loading.png" alt="Loading" className="w-full h-[100vh] object-cover" />
+      <p className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl font-bold">
+        Carregando...
+      </p>
+    </div>
+    )
   }
 
   // Cria um array na ordem de selectedCharacters e filtra apenas os que existem na lista de characters
-  const orderedCharacters = selectedCharacters.map(id => 
-    characters.find(character => character.id === id)
-  ).filter(character => character !== undefined); // Remove qualquer personagem não encontrado
+  const orderedCharacters = selectedCharacters
+    .map((id) => characters.find((character) => character.id === id))
+    .filter((character) => character !== undefined); // Remove qualquer personagem não encontrado
 
   const getImageSrc = (characterID) => {
-    const extensions = ['jpg','png','webp']
-    for(let ext of extensions){
+    const extensions = ["jpg", "png", "webp"]
+    for (let ext of extensions) {
       const path = `/images/wanted/${characterID}.${ext}`
-      if(typeof window!=undefined){
-        const img = new Image()
-        img.src = path
-        if(img.complete){
-          return path
+      if (typeof window != undefined) {
+        const img = new Image();
+        img.src = path;
+        if (img.complete) {
+          return path;
         }
-      }      
+      }
     }
-    return '/images/wanted/1.webp';
-  }
+    return "/images/wanted/1.webp";
+  };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-red-500 via-yellow-300 to-pink-800 relative">
-      <div className="flex py-8 ml-20">
-        <img src="/images/ROGER.png" alt="Rei dos Piratas" className="w-auto h-[90vh] ml-20 object-cover"/>
-      <h1 style={{ fontFamily: 'Times New Roman', fontWeight: 'bold' }} className="text-white text-5xl ml-20 mt-5">PERSONAGENS DE ONE PIECE <br/> COM AS MAIORES RECOMPENSAS</h1>
+    <section className="flex flex-col h-full bg-gradient-to-br relative from-red-500 via-yellow-300 to-pink-800">
+      <div className="fixed z-0 flex py-8 ml-20">
+        <img
+          src="/images/ROGER.png"
+          alt="Rei dos Piratas"
+          className="w-auto h-[90vh] ml-20 object-cover"
+        />
       </div>
-      <div className="absolute mt-[10%] top-10 w-full flex flex-wrap justify-center">
-      {orderedCharacters.length > 0 ? (
+        <h1
+          style={{ fontFamily: "Times New Roman", fontWeight: "bold" }}
+          className="text-white text-4xl mt-10 absolute right-20"
+        >
+          PERSONAGENS DE ONE PIECE <br/> COM AS MAIORES RECOMPENSAS
+        </h1>
+      <div className="mt-[12%] top-10 w-full flex flex-wrap justify-center z-10">
+        {orderedCharacters.length > 0 ? (
           orderedCharacters.map((character) => (
-            <div key={character.id} className="w-64 h-auto mt-[2%] m-4 p-5 bg-[#DFC6B0] rounded shadow-lg hover:shadow-2xl hover:scale-110 transition-transform duration-300"> 
-            <h2 style={{ fontFamily: 'Times New Roman', fontWeight: 'bold' }}>WANTED</h2>
-            <img src={getImageSrc(character.id)} className="w-64 h-auto"/>
-            <h2>DEAD OR ALIVE</h2>
-            <h2 className="text-lg font-bold">{character.name}</h2>
-            <p>{character.job}</p>
-            <h2>{character.bounty}</h2>
-          </div>
+            <div
+              key={character.id}
+              className="cards"
+            >
+              <h2 style={{ fontFamily: "Times New Roman", fontWeight: "bold" }} className="text-2xl">
+                WANTED
+              </h2>
+              <img src={getImageSrc(character.id)} className="w-64 h-auto" />
+              <h2 style={{ fontFamily: "Times New Roman", fontWeight: "bold" }}>DEAD OR ALIVE</h2>
+              <h2 className="text-name">{character.name}</h2>
+              <p>{character.job}</p>
+              <h2>{character.bounty}</h2>
+            </div>
           ))
-      ) : (
-        <p>Nenhum personagem encontrado.</p>
-      )}
-    </div>
-    </div>
+        ) : (
+          <p>Nenhum personagem encontrado.</p>
+        )}
+      </div>
+    </section>
   );
 }
-
-// import { useState, useEffect } from 'react';
-
-// export default function Characters() {
-//   const [characters, setCharacters] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [selectedCharacters, setSelectedCharacters] = useState([258, 289, 187, 96, 85, 247, 81, 32, 1, 54, 59, 82]); // IDs dos personagens que você deseja mostrar (como números)
-
-//   useEffect(() => {
-//     const url = 'https://api.api-onepiece.com/v2/characters/en';
-
-//     const fetchData = async () => {
-//       try {
-//         const response = await fetch(url);
-//         if (!response.ok) {
-//           throw new Error('Erro na requisição');
-//         }
-//         const result = await response.json();
-//         console.log(result); // Verifique o que está sendo retornado
-//         setCharacters(result);
-//         setLoading(false);
-//       } catch (error) {
-//         console.error('Erro ao buscar os dados:', error);
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   if (loading) {
-//     return <p>Carregando...</p>;
-//   }
-
-//   // Filtra personagens com base nos IDs selecionados
-//   const filteredCharacters = characters.filter(character => selectedCharacters.includes(character.id)); // Comparando como números
-
-//   return (
-//     <div>
-//       <h1>Personagens de One Piece</h1>
-//       {filteredCharacters.length > 0 ? (
-//         <ul>
-//           {filteredCharacters.map((character) => (
-//             <li key={character.id}>
-//               {character.name} - {character.job}
-//             </li>
-//           ))}
-//         </ul>
-//       ) : (
-//         <p>Nenhum personagem encontrado.</p>
-//       )}
-//     </div>
-//   );
-// }
-
--- src/pages/list.js
+++ src/pages/list.js
@@ -17,15 +17,15 @@ export default function Characters() {
         const result = await response.json();
         console.log(result); // Para verificar a resposta da API
         setCharacters(result); // Armazenando os dados diretamente, já que é um array
-        setLoading(false);
+        setLoading(false)
       } catch (error) {
-        console.error('Erro ao buscar os dados:', error);
+        console.error('Erro ao buscar os dados:', error)
         setLoading(false);
       }
-    };
+    }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   // Função para filtrar personagens pelo nome
   const filteredCharacters = characters.filter(character =>
@@ -33,29 +33,43 @@ export default function Characters() {
   );
 
   if (loading) {
-    return <p>Carregando...</p>;
+    return (
+      <div className="flex items-center justify-center min-h-screen relative">
+      <img src="/images/loading.png" alt="Loading" className="w-full h-[100vh] object-cover" />
+      <p className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl font-bold">
+        Carregando...
+      </p>
+    </div>
+    )
   }
 
   return (
-    <div>
-      <h1>Personagens de One Piece</h1>
+    <section className="bg-[url('/images/index.png')] bg-cover h-[100vh]">
+    <div className="flex items-center justify-between">
+      <img src="/images/logo.png" className="mt-10 ml-5"></img>
       <input
         type="text"
-        placeholder="Pesquisar personagem pelo nome..."
+        placeholder="Pesquisar personagens..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)} // Atualiza o estado com o valor do input
-      />
-      {filteredCharacters.length > 0 ? (
-        <ul>
-          {filteredCharacters.map((character) => (
-            <li key={character.id}>
-              {character.name} - {character.job} - {character.id}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Nenhum personagem encontrado.</p>
-      )}
+        className="mt-10 mr-5 border bg-[#BB5346] border-black text-white rounded-full p-2 mb-4 w-3/4 max-w-sm" // Estilos para a barra de pesquisa
+    />
     </div>
+    <h1 className="text-7xl text-white font-bold mt-10 flex justify-center mb-4">ONE API</h1>
+    {/* Mostrar a listagem apenas se houver um termo de pesquisa */}
+    {searchTerm && filteredCharacters.length > 0 ? (
+      <ul className="w-full max-w-md">
+        {filteredCharacters.map((character) => (
+          <li key={character.id} className="border-b p-2 text-white">
+            {character.name} - {character.job}
+          </li>
+        ))}
+      </ul>
+    ) : (
+      searchTerm && <p>Nenhum personagem encontrado.</p> // Mensagem se não encontrar
+    )}
+    <div>
+  </div>
+  </section>
   );
 }
